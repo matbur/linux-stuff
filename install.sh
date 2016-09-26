20024ce89ba7dee1a5a7f49ce989d733034f0d66
matbur95 <mateusz@burniak.pl>
2016-09-27 00:48:19 +0200

    add bin

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ create_ln .vimrc
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+create_ln .bashrc
 create_ln .zshrc
 create_ln .git-completion.bash
 create_ln .aliases.sh
