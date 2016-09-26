d4a91ee6fa9bf5793fa4913526b8c577645336d8
matbur95 <mateusz@burniak.pl>
2016-09-27 01:08:30 +0200

    better symlink

--- install.sh
+++ install.sh
@@ -4,6 +4,10 @@ function create_ln() {
     ln -sf ${PWD}/$1 ${HOME}/$1
 }
 
+function create_ln_dir() {
+    ln -sfn ${PWD}/$1 ${HOME}/$1
+}
+
 create_ln .vimrc
 curl -sfLo ~/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
@@ -17,6 +21,6 @@ create_ln .aliases.sh
 create_ln .i3blocks.conf
 create_ln .config/i3/config
 
-create_ln bin
+create_ln_dir bin
 
-create_ln .ptpython
+create_ln_dir .ptpython
