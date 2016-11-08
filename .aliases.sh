151fffba92da248acbe94c1b6f08eef6feb2a4c4
matbur95 <mateusz@burniak.pl>
2016-11-08 13:16:02 +0100

    Add o alias for xgd-open

--- .aliases.sh
+++ .aliases.sh
@@ -22,6 +22,7 @@ alias aliases='vim ~/.aliases.sh'
 #   sleep 10; alert
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
 
+alias o='xdg-open'
 alias py='python'
 alias py3='python3'
 alias ptp='ptpython'
